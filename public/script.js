fd15eaf436d8a3943095d0028f05368f2e812792
Thomas Feuerborn <tfeuerbo@gmail.com>
2016-11-13 16:52:55 -0700

    Fixed EOI error (Take Two)

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
+var socket = io();
 $('document').ready(function () {
-    
-    var socket = io();
     $('#client_info').submit(function (evt) {
         evt.preventDefault();
         var temp = '';
         socket.emit('get clients', temp);
     });
+});
