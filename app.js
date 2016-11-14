15c7402e2d3c281c969ded5e1fca0a9e10438369
Thomas Feuerborn <tfeuerbo@gmail.com>
2016-11-13 17:58:54 -0700

    added dynamic room system
    Signed-off-by: Thomas Feuerborn <tfeuerbo@gmail.com>

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 
 var MAX_USERS = 4;
-var rooms = [];
 /* make rooms */
-
+var rooms = [];
+var s = new Date().toString();
+console.log(s);
+rooms.push(s);
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
@@ -16,8 +18,37 @@ app.get('/', function (req, res) {
 
 io.on('connection', function (socket) {
     /* do stuff */
-    console.log(new Date());
-
+    socket.join('waiting room');
+    var createNewRoom = true;
+    var i;
+    for (i = 0; i < rooms.length; i++) {
+        console.log(rooms[i]);
+        console.log("\t%j", io.sockets.adapter.rooms['waiting room']);
+        console.log("\t\t%j", io.sockets.adapter.rooms[rooms[i]]);
+        if (io.sockets.adapter.rooms[rooms[i]] === undefined) {
+            socket.join(rooms[i]);
+            console.log("\t\t%j", io.sockets.adapter.rooms[rooms[i]]);
+            createNewRoom = false;
+        } else {
+            if (io.sockets.adapter.rooms[rooms[i]].length < MAX_USERS) {
+                console.log("Open Space for new User");
+                socket.join(rooms[i]);
+                console.log("\t\t%j", io.sockets.adapter.rooms[rooms[i]]);
+                createNewRoom = false;
+                /* break out of loop */
+                i = Number.MAX_SAFE_INTEGER;
+            } else {
+                console.log("Room is Full");
+            }
+        }
+    }
+    if (createNewRoom) {
+        console.log("Making new room");
+        var roomName = new Date().toString();
+        rooms.push(roomName);
+        socket.join(roomName);
+        console.log("%j", io.sockets.adapter.rooms[roomName]);
+    }
 });
 
 http.listen(PORT, function () {
