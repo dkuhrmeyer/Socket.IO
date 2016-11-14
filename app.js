1f3d5d36cf633688ad064e5964f9cd050c051f10
Daniel <dkuhrmeyer@gmail.com>
2016-11-13 18:00:58 -0700

    set up when someone disconnects

--- app.js
+++ app.js
@@ -15,10 +15,19 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
+    console.log(socket.id + "user has connected");
+    socket.on("disconnect", function(){
+        console.log(socket.id + "user has disconnected")
+    });
     /* do stuff */
+    console.log(io.sockets.clients());
+    io.emit('list clients', io.sockets.clients().adapter.sids);
     console.log(new Date());
 
 });
+
+
+
 
 http.listen(PORT, function () {
     console.log("listening on localhost:" + PORT);
--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
 var socket = io();
+
 $('document').ready(function () {
     $('#client_info').submit(function (evt) {
         evt.preventDefault();
         var temp = '';
         socket.emit('get clients', temp);
     });
+});
+
+socket.on("list clients", function (data){
+    console.log(data);
 });
