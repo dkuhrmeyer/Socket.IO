477424c517bc5a3acf8ea63ee5df94f1810292ee
Daniel <dkuhrmeyer@gmail.com>
2016-11-16 08:10:55 -0700

    made an extra js file to decouple our create.js

---
+++ public/setup.js
@@ -0,0 +1,36 @@
+var stage, queue;
+var manifest = [{
+    
+}];
+
+function setupCanvas(){
+    var canvas = document.getElementById("game");
+    canvas.width = 800;
+    canvas.height = 600;
+    stage = new createjs.Stage(canvas);
+}
+
+function loadComplete(evt){
+    GAME_STATES.STATE = GAME_STATES.INIT;
+}
+
+function loadFiles(){
+    queue = new createjs.LoadQueue(true, "assets/");
+    queue.on("complete", loadComplete, this);
+    queue.installPlugin(createjs.Sound);
+    queue.loadManifest(manifest);
+}
+
+(function main() { //Main
+    setupCanvas();
+    loadFiles();
+})();
+
+
+$('document').ready(function () {
+    $('#client_info').submit(function (evt) {
+        evt.preventDefault();
+        var temp = '';
+        socket.emit('get clients', temp);
+    });
+});
